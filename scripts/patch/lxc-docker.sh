2bb6dda6b267d76d402596c523f74665742fb845
5kind <weirui5671@outlook.com>
2023-12-17 22:37:38 +0800

    disable cgroup.patch

--- scripts/patch/lxc-docker.sh
+++ scripts/patch/lxc-docker.sh
@@ -4,8 +4,8 @@ patchf(){
     [ -f $file ] && patch -p1 < $patch
 }
 
-patchf kernel/cgroup.c $LXC_PATCHES/cgroup.patch
-patchf kernel/cgroup/cgroup.c $LXC_PATCHES/cgroup.patch
+# patchf kernel/cgroup.c $LXC_PATCHES/cgroup.patch
+# patchf kernel/cgroup/cgroup.c $LXC_PATCHES/cgroup.patch
 patchf net/netfilter/xt_qtaguid.c $LXC_PATCHES/xt_qtaguid.patch
 # patchf include/linux/mm.h $LXC_PATCHES/mm.patch
 # patchf kernel/cgroup/cpuset.c $LXC_PATCHES/cpuset.patch
