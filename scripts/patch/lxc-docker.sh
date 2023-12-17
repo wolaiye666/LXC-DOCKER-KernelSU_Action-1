065b11968a5062fa8d2c0144e3e63d66fc4a936f
5kind <weirui5671@outlook.com>
2023-12-17 14:40:37 +0800

    patch scripts

---
+++ scripts/patch/lxc-docker.sh
@@ -0,0 +1,11 @@
+patchf(){
+    local file=$1
+    local patch=$2
+    [ -f $file ] && patch -p1 < $patch
+}
+
+patchf kernel/cgroup.c $LXC_PATCHES/cgroup.patch
+patchf kernel/cgroup/cgroup.c $LXC_PATCHES/cgroup.patch
+patchf net/netfilter/xt_qtaguid.c $LXC_PATCHES/xt_qtaguid.patch
+patchf include/linux/mm.h $LXC_PATCHES/mm.patch
+patchf kernel/cgroup/cpuset.c $LXC_PATCHES/cpuset.patch
