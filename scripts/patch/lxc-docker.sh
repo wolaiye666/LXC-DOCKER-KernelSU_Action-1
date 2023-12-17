c2e0ff9cddd9747c89ee71402d0186c2e83f3085
5kind <weirui5671@outlook.com>
2023-12-17 22:07:13 +0800

    disable mm.patch cpuset.patch

--- scripts/patch/lxc-docker.sh
+++ scripts/patch/lxc-docker.sh
@@ -7,5 +7,5 @@ patchf(){
 patchf kernel/cgroup.c $LXC_PATCHES/cgroup.patch
 patchf kernel/cgroup/cgroup.c $LXC_PATCHES/cgroup.patch
 patchf net/netfilter/xt_qtaguid.c $LXC_PATCHES/xt_qtaguid.patch
-patchf include/linux/mm.h $LXC_PATCHES/mm.patch
-patchf kernel/cgroup/cpuset.c $LXC_PATCHES/cpuset.patch
+# patchf include/linux/mm.h $LXC_PATCHES/mm.patch
+# patchf kernel/cgroup/cpuset.c $LXC_PATCHES/cpuset.patch
